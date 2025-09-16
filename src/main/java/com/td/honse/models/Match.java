83e8e57ab7ec0db2254adaf4c29bebf560a8a0bd
Wudanty <wudanty@gmail.com>
2025-09-16 12:59:42 +0200

    remove equalsandhashcode annotation from the class

--- src/main/java/com/td/honse/models/Match.java
+++ src/main/java/com/td/honse/models/Match.java
@@ -1,21 +1,16 @@
 package com.td.honse.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "matches")
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @ToString(exclude = {"scores", "trainees"})
 public class Match {
     @Id
